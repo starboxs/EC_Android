8c4f53591c2b40d19ba4064d354b78314fe6d99d
Marco <starboxs8917@gmail.com>
2018-12-22 15:10:11 +0800

    吾皇

--- app/src/main/java/com/example/marco/ec_android/Conf.java
+++ app/src/main/java/com/example/marco/ec_android/Conf.java
@@ -8,6 +8,6 @@ public class Conf {
 
     // Api
     public static final String API_TEST_BASE_URL = "韓國瑜甲賽";
-    public static final String API_REAL_BASE_URL = "韓國瑜甲賽";
+    public static final String API_REAL_BASE_URL = "甲賽韓國瑜";
 
 }
